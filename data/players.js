cff0dc7a05f9fef75a87fb73420d49543042f487
MysticalFinds <cristoferparm21@gmail.com>
2026-08-07 15:12:12 +0300

    Add player data to players.js
    Added player data for goalkeepers, defenders, midfielders, and attackers.

--- data/players.js
+++ data/players.js
@@ -1,1 +1,29 @@
+const players = [
+    // Goalkeepers
+    {name:"Player 1",position:"Goalkeeper",captain:false,vice:false},
+    {name:"Player 2",position:"Goalkeeper",captain:false,vice:false},
 
+    // Defenders
+    {name:"Player 3",position:"Defender",captain:false,vice:false},
+    {name:"Player 4",position:"Defender",captain:true,vice:false},
+    {name:"Player 5",position:"Defender",captain:false,vice:true},
+    {name:"Player 6",position:"Defender",captain:false,vice:false},
+    {name:"Player 7",position:"Defender",captain:false,vice:false},
+    {name:"Player 8",position:"Defender",captain:false,vice:false},
+
+    // Midfielders
+    {name:"Player 9",position:"Midfielder",captain:false,vice:false},
+    {name:"Player 10",position:"Midfielder",captain:false,vice:false},
+    {name:"Player 11",position:"Midfielder",captain:false,vice:false},
+    {name:"Player 12",position:"Midfielder",captain:false,vice:false},
+    {name:"Player 13",position:"Midfielder",captain:false,vice:false},
+
+    // Attackers
+    {name:"Player 14",position:"Attacker",captain:false,vice:false},
+    {name:"Player 15",position:"Attacker",captain:false,vice:false},
+    {name:"Player 16",position:"Attacker",captain:false,vice:false},
+    {name:"Player 17",position:"Attacker",captain:false,vice:false},
+    {name:"Player 18",position:"Attacker",captain:false,vice:false},
+    {name:"Player 19",position:"Attacker",captain:false,vice:false},
+    {name:"Player 20",position:"Attacker",captain:false,vice:false},
+];
